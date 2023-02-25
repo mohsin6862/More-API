614d9335131c36197f4558ecc6b8965a3cd7e4d1
mohsin6862 <mohsinctg2206@gmail.com>
2023-02-25 08:42:02 +0600

    Country API added

---
+++ js/country.js
@@ -0,0 +1,37 @@
+const getCountry = ()=>{
+
+    fetch('https://restcountries.com/v2/all')
+    .then(res => res.json())
+    .then(data => displayCountry(data.slice(0,6)))
+}
+
+const displayCountry = (countries)=> {
+     const countryContainer = document.getElementById('country-container');
+    //  countryContainer = '';
+    countries.forEach(country => {
+      
+        console.log(country)
+       const div = document.createElement('div');
+       div.innerHTML = `
+       <div class="card w-full bg-white shadow-2xl">
+       <figure class="px-10 pt-10">
+         <img class="w-full h-40" src="${country.flags.png}" alt="Shoes" class="rounded-xl" />
+       </figure>
+       <div class="card-body items-center text-center">
+         <h2 class="card-title text-black font-bold text-2xl">${country.name}</h2>
+         <h3 class="card-title text-black font-bold text-xl">Capital: ${country.capital}</h3>
+         <h4 class="card-title text-black font-bold text-lg">Region: ${country.region}</h4>
+         <h4 class="card-title text-black font-bold text-lg">Area: ${country.area}</h4>
+         
+      
+         <div class="card-actions">
+          
+         <button class="btn btn-primary">details</button>
+         </div>
+       
+       `
+       countryContainer.appendChild(div);
+        
+    });
+}
+getCountry();
